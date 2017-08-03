24c37b84c176f03b7b005c29423e1e05f41c8799
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-08-03 23:00:43 +0200

    Reverted to /tmp

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -30,7 +30,7 @@ fi # [ -f requirements.txt ]
 echo "$@"
 
 if [[ "$@" == "" ]]; then
-    pyinstaller --clean -y --dist ./dist/windows --workpath C:\\tmp *.spec
+    pyinstaller --clean -y --dist ./dist/windows --workpath /tmp *.spec
     chown -R --reference=. ./dist/windows
 else
     sh -c "$@"
