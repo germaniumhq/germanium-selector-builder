224f3f7676af8b9cba7cffb7a36893f181243056
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-09-28 04:57:45 +0200

    Moved the windows build to arst.

--- _gbs/win32/_gbs/build/prepare-build1/prepare-build1.sh
+++ _gbs/win32/_gbs/build/prepare-build1/prepare-build1.sh
@@ -1,4 +1,8 @@
 wine /germanium/wine/drive_c/Miniconda3/Scripts/pip.exe install PyInstaller
+wine /germanium/wine/drive_c/Miniconda3/Scripts/pip.exe install -U pip setuptools
+
 
 # Pyside it's probably not going to change that often, so better cache it as well
 wine /germanium/wine/drive_c/Miniconda3/Scripts/conda.exe install -c conda-forge pyside2
+
+
