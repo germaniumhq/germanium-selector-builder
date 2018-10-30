ae5d4cc91124d03d0070fbea2ce3cdf64f29c8e6
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-30 05:51:37 +0100

    Try 3.3.1 of pyinstaller.

--- _gbs/win32/_gbs/prepare-build1/prepare-build1.sh
+++ _gbs/win32/_gbs/prepare-build1/prepare-build1.sh
@@ -1,4 +1,4 @@
-wine /germanium/wine/drive_c/Miniconda3/Scripts/pip.exe install PyInstaller==3.2.1
+wine /germanium/wine/drive_c/Miniconda3/Scripts/pip.exe install PyInstaller==3.3.1
 wine /germanium/wine/drive_c/Miniconda3/Scripts/pip.exe install -U pip setuptools
 
 
