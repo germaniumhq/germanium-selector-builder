2eb4ec5cc870de5a05ea19412012a63917ff3b41
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-11-16 05:58:31 +0100

    Updated stubs.

--- stubs/PySide2/QtGui/__init__.pyi
+++ stubs/PySide2/QtGui/__init__.pyi
@@ -1,3 +1,6 @@
+from PySide2.QtCore import QSize
+
+
 class QPixmap:
     def __init__(self,
                  icon_path: str) -> None:
@@ -14,6 +17,9 @@ class QIcon:
                   flag2: int) -> None:
         pass
 
+    def pixmap(self, size: QSize) -> QPixmap:
+        pass
+
 
 class QTextCharFormat:
     pass
@@ -25,3 +31,12 @@ class QSyntaxHighlighter:
 
 class QFont:
     pass
+
+
+class QMovie:
+    def __init__(self,
+                 movie_path: str) -> None:
+        pass
+
+    def start(self) -> None:
+        pass
